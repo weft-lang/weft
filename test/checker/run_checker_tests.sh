192cd405eb530223bf8fc87dc97f5a4486e95af5
Christopher Grainger <chris@amplified.ai>
2026-04-29 23:05:14 +1000

    test: cover trait associated type diagnostics

--- test/checker/run_checker_tests.sh
+++ test/checker/run_checker_tests.sh
@@ -34,6 +34,7 @@ check_accepts "function_value_pure_call" "test/checker/function_value_pure_call.
 check_accepts "function_value_effect_call" "test/checker/function_value_effect_call.weft"
 check_accepts "method_calls" "test/checker/method_calls.weft"
 check_accepts "trait_impl_signatures" "test/checker/trait_impl_signatures.weft"
+check_accepts "trait_associated_types" "test/checker/trait_associated_types.weft"
 check_accepts "let_bound_lambdas" "test/checker/let_bound_lambdas.weft"
 check_accepts "name_resolution" "test/checker/name_resolution.weft"
 check_accepts "field_access" "test/checker/field_access.weft"
