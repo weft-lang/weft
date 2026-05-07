a25f4278821edffae48c231e825d3c1c0eb2e4de
Christopher Grainger <chris@amplified.ai>
2026-05-07 17:38:11 +1000

    feat: add unsafe runtime register intrinsics

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -37,6 +37,8 @@ check_rejects "unsafe_raw_offset_requires_effect" "test/negative/unsafe_raw_offs
 check_rejects "unsafe_transmute_requires_effect" "test/negative/unsafe_transmute_requires_effect.weft" "type error: effect not available in caller"
 check_rejects "unsafe_int_to_ptr_requires_effect" "test/negative/unsafe_int_to_ptr_requires_effect.weft" "type error: effect not available in caller"
 check_rejects "unsafe_runtime_state_requires_effect" "test/negative/unsafe_runtime_state_requires_effect.weft" "type error: effect not available in caller"
+check_rejects "unsafe_set_runtime_state_requires_effect" "test/negative/unsafe_set_runtime_state_requires_effect.weft" "type error: effect not available in caller"
+check_rejects "unsafe_set_handler_stack_requires_effect" "test/negative/unsafe_set_handler_stack_requires_effect.weft" "type error: effect not available in caller"
 check_rejects "unsafe_raw_call_i64_requires_effect" "test/negative/unsafe_raw_call_i64_requires_effect.weft" "type error: effect not available in caller"
 check_rejects "unsafe_got_requires_effect" "test/negative/unsafe_got_requires_effect.weft" "type error: effect not available in caller"
 check_rejects "unsafe_imported_got_requires_effect" "test/negative/unsafe_imported_got_requires_effect.weft" "type error: effect not available in caller"
