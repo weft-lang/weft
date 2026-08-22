7ab7f275aa0dd958051f84a09be10ad1870d034a
Christopher Grainger <chris@amplified.ai>
2026-08-22 13:32:10 +1000

    feat: make source acquisition a pipeline effect

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -83,6 +83,7 @@ check_rejects "generator_generic_new_mutable_closure_producer" "test/negative/ge
 check_rejects "generator_generic_new_return_type_mismatch" "test/negative/generator_generic_new_return_type_mismatch.weft" 'error[E1002]: lambda return value type mismatch: expected `i64`, found `str`'
 check_rejects "generator_generic_yield_unhandled" "test/negative/generator_generic_yield_unhandled.weft" "error[E2001]:"
 check_rejects "unhandled_effect_perform" "test/negative/unhandled_effect_perform.weft" 'error[E2001]: effect `State` is not available in this context'
+check_rejects "source_acquire_requires_effect" "test/negative/source_acquire_requires_effect.weft" 'error[E2001]: effect `SourceAcquire` is not available in this context'
 check_rejects "secure_random_effect_unavailable" "test/negative/secure_random_effect_unavailable.weft" 'error[E2001]: effect `SecureRandom` is not available in this context'
 check_rejects "secure_random_raw_backend_private" "test/negative/secure_random_raw_backend_private.weft" "module member 'runtime_secure_random_fill_raw' is not visible in this import" 1
 check_rejects "net_address_constructor_private" "test/negative/net_address_constructor_private.weft" "type error: opaque constructor is private to its declaring module; use an exported factory"
