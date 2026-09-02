918d822fa76aa7384440dd2ab48e6cc47ff461a0
Christopher Grainger <chris@amplified.ai>
2026-09-03 07:19:13 +1000

    refactor: make numeric widening receiver owned

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -621,7 +621,7 @@ check_rejects "num_to_i64_exact_wrong_receiver" "test/negative/num_to_i64_exact_
 check_rejects "num_parse_f64_default_mismatch" "test/negative/num_parse_f64_default_mismatch.weft" 'error[E1002]: argument type mismatch: expected `f64`, found `str`'
 check_rejects "num_parse_float_lane_mismatch" "test/negative/num_parse_float_lane_mismatch.weft" 'error[E1002]: type annotation type mismatch: expected `Result<f64, num.NumParseError>`, found `Result<f32, num.NumParseError>`'
 check_rejects "intrinsic_f64_to_i64_arg_mismatch" "test/negative/intrinsic_f64_to_i64_arg_mismatch.weft" 'error[E1002]: argument type mismatch: expected `f64`, found `i64`'
-check_rejects "num_f32_to_f64_arg_mismatch" "test/negative/num_f32_to_f64_arg_mismatch.weft" 'error[E1002]: argument type mismatch: expected `f32`, found `f64`'
+check_rejects "num_to_f64_receiver_mismatch" "test/negative/num_to_f64_receiver_mismatch.weft" "unknown method"
 check_rejects "num_f64_to_f32_round_arg_mismatch" "test/negative/num_f64_to_f32_round_arg_mismatch.weft" 'error[E1002]: argument type mismatch: expected `f64`, found `i64`'
 check_rejects "num_i32_to_f32_round_arg_mismatch" "test/negative/num_i32_to_f32_round_arg_mismatch.weft" 'error[E1002]: argument type mismatch: expected `i32`, found `u32`'
 check_rejects "intrinsic_f32_to_f64_arg_mismatch" "test/negative/intrinsic_f32_to_f64_arg_mismatch.weft" 'error[E1002]: argument type mismatch: expected `f32`, found `f64`'
@@ -643,7 +643,7 @@ check_rejects "state_surface_retired" "test/negative/state_surface_retired.weft"
 check_rejects "effect_interpreters_retired" "test/negative/effect_interpreters_retired.weft" "unknown module member" 2
 check_rejects "ord_compare_retired" "test/negative/ord_compare_retired.weft" "unknown module member" 1
 check_rejects "num_trait_forwarders_retired" "test/negative/num_trait_forwarders_retired.weft" "unknown module member" 9
-check_rejects "num_value_functions_retired" "test/negative/num_value_functions_retired.weft" "unknown module member" 8
+check_rejects "num_value_functions_retired" "test/negative/num_value_functions_retired.weft" "unknown module member" 17
 check_rejects "num_parse_functions_retired" "test/negative/num_parse_functions_retired.weft" "unknown module member" 6
 check_rejects "mini_sql_grammar_retired" "test/negative/mini_sql_grammar_retired.weft" "unknown module member" 1
 check_rejects "f64_table_prefixed_surface_retired" "test/negative/f64_table_prefixed_surface_retired.weft" "in import" 4
