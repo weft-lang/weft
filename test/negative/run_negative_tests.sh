8d8dd58897814d1a5ba9c3ca4ed52f7f68480f97
Christopher Grainger <chris@amplified.ai>
2026-09-02 23:17:37 +1000

    refactor: consolidate console handler modules

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -555,6 +555,7 @@ check_rejects "file_read_cannot_write" "test/negative/file_read_cannot_write.wef
 check_rejects "dir_inspect_cannot_mutate" "test/negative/dir_inspect_cannot_mutate.weft" "error[E2001]:"
 check_rejects "dir_prefixed_list_removed" "test/negative/dir_prefixed_list_removed.weft" "error[E4002]: unknown module member 'dir_list' in import" 1
 check_rejects "console_cannot_write_file" "test/negative/console_cannot_write_file.weft" "error[E2001]:"
+check_rejects "console_platform_wrappers_retired" "test/negative/console_platform_wrappers_retired.weft" "error[E4002]: unknown module member 'runtime_platform_console_read' in import" 4
 check_rejects "safe_io_platform_residual_effect" "test/negative/safe_io_platform_residual_effect.weft" "error[E2001]:"
 check_rejects "if_condition_not_bool" "test/negative/if_condition_not_bool.weft" "type error: boolean expression is not bool"
 check_rejects "while_condition_not_bool" "test/negative/while_condition_not_bool.weft" "type error: boolean expression is not bool"
