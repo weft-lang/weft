ac7a9a3e78af34c063f8efd437c8b813ca877849
Christopher Grainger <chris@amplified.ai>
2026-09-03 06:17:54 +1000

    refactor: simplify collection constructors

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -636,6 +636,7 @@ check_rejects "unicode_prefixed_surface_retired" "test/negative/unicode_prefixed
 check_rejects "iterator_free_functions_retired" "test/negative/iterator_free_functions_retired.weft" "unknown module member" 10
 check_rejects "generator_functions_retired" "test/negative/generator_functions_retired.weft" "unknown module member" 8
 check_rejects "channel_functions_retired" "test/negative/channel_functions_retired.weft" "unknown module member" 4
+check_rejects "collection_constructors_retired" "test/negative/collection_constructors_retired.weft" "unknown module member" 4
 check_rejects "state_surface_retired" "test/negative/state_surface_retired.weft" "unknown module member" 4
 check_rejects "effect_interpreters_retired" "test/negative/effect_interpreters_retired.weft" "unknown module member" 2
 check_rejects "ord_compare_retired" "test/negative/ord_compare_retired.weft" "unknown module member" 1
