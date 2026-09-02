52e297bedb2b9a7a7537646d3e3c46724a015050
Christopher Grainger <chris@amplified.ai>
2026-09-02 19:24:32 +1000

    refactor: expose files through namespaces and handles

--- test/negative/run_negative_tests.sh
+++ test/negative/run_negative_tests.sh
@@ -490,6 +490,7 @@ check_rejects "bytes_prefixed_constructor_removed" "test/negative/bytes_prefixed
 check_rejects "path_constructor_private" "test/negative/path_constructor_private.weft" "type error: opaque constructor is private to its declaring module; use an exported factory"
 check_rejects "path_prefixed_constructor_removed" "test/negative/path_prefixed_constructor_removed.weft" "error[E4002]: unknown module member 'path_from_utf8' in import" 1
 check_rejects "io_helper_effect_unavailable" "test/negative/io_helper_effect_unavailable.weft" "error[E2001]:"
+check_rejects "file_prefixed_read_removed" "test/negative/file_prefixed_read_removed.weft" "error[E4002]: unknown module member 'file_read_all' in import" 1
 check_rejects "file_read_cannot_write" "test/negative/file_read_cannot_write.weft" "error[E2001]:"
 check_rejects "dir_inspect_cannot_mutate" "test/negative/dir_inspect_cannot_mutate.weft" "error[E2001]:"
 check_rejects "dir_prefixed_list_removed" "test/negative/dir_prefixed_list_removed.weft" "error[E4002]: unknown module member 'dir_list' in import" 1
